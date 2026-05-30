333860abb555ec7e4d1ba2c3428e369cfab7c034
Hunter09090 <aakterhossen80@gmail.com>
2026-05-30 11:43:47 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
-// ওয়েবসাইট লোড হওয়ার পর কিছু অ্যানিমেশন বা মেসেজ
-window.onload = () => {
-    console.log("আল-হারামাইন ক্যাম্পাস ওয়েবসাইট সফলভাবে লোড হয়েছে!");
-    
-    // বাটন ক্লিক ইভেন্ট
-    const btn = document.querySelector('button');
-    if(btn) {
-        btn.addEventListener('click', () => {
-            alert('ওয়েবসাইটে স্বাগতম!');
-        });
-    }
-};
+// ওয়েবসাইট লোড হওয়ার সময় একটি সুন্দর কনসোল মেসেজ
+console.log("আল-হারামাইন ক্যাম্পাস ওয়েবসাইট লোড হয়েছে");
+
+// বাটন বা ফর্ম সাবমিট করার সময় ছোট এনিমেশন
+document.querySelector('form')?.addEventListener('submit', (e) => {
+    e.preventDefault();
+    alert('এটি একটি ডেমো লগইন! ডেটাবেস কানেকশনের কাজ চলছে...');
+});
